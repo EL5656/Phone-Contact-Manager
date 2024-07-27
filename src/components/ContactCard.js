93976a65945ec4f4d6acdf08c0c56f5b3164894e
elaine460 <102460032+elaine460@users.noreply.github.com>
2024-07-28 03:45:17 +0800

    Refactor

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -3,14 +3,15 @@ import { FaTrashAlt, FaEdit } from 'react-icons/fa';
 
 const ContactCard = ({ contact }) => {
     const greyStyle = { color: 'grey' };
+    const {id, name, email, mobile} = contact;
 
     return (
         <div className="col-md-4 mb-4">
             <div className="card">
                 <div className="card-body">
-                    <h4 className="card-title">{contact.name}</h4>
-                    <p className="card-text mb-1"><strong>Email:</strong> {contact.email}</p>
-                    <p className="card-text mb-1"><strong>Phone:</strong> {contact.mobile}</p>
+                    <h4 className="card-title">{name}</h4>
+                    <p className="card-text mb-1"><strong>Email:</strong> {email}</p>
+                    <p className="card-text mb-1"><strong>Phone:</strong> {mobile}</p>
                     <div className="d-flex justify-content-end mt-3">
                         <button className="btn btn-link text-info btn-sm me-2" aria-label="Edit">
                             <FaEdit size={20} style={greyStyle} />
